637ef44640d530502755424c4966b23bc68d9f97
pedrogg0 <pedrojosegarcia00@gmail.com>
2023-09-22 19:52:17 +0200

    init

--- js/index.js
+++ js/index.js
@@ -1,54 +1,87 @@
-// Create the application helper and add its render target to the page
-const app = new PIXI.Application({width: 800, height: 400});
-$("#element").html(app.view);
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 400,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
+var game = new Phaser.Game(config);
 
+function preload ()
+{
+    this.load.image("background", 'assets/foreground.png');
+    this.load.spritesheet("idle", "assets/idle.png", { frameWidth: 48, frameHeight: 48 });
+    this.load.spritesheet("jump", "assets/jump.png", { frameWidth: 48, frameHeight: 48 });
+    this.load.spritesheet("land", "assets/land.png", { frameWidth: 48, frameHeight: 48 });
+    this.load.spritesheet("run", "assets/run.png", { frameWidth: 48, frameHeight: 48 });
+}
 
-PIXI.Assets.load([
-    "assets/spritesheet.json",
-]).then(() => {
-    function drawAnimation(animation){ //idle,run,jump
-        // get the sheet json data, required for resolving animations
-        const animations = PIXI.Assets.cache.get("assets/spritesheet.json").data.animations;
-        // create an animated sprite
-        const character = PIXI.AnimatedSprite.fromFrames(animations[animation]);
-        // configure + start animation:
-        character.animationSpeed = 1 / 6;                     // 6 fps
-        character.play();
+function create ()
+{
+    //Background creation
+    background = this.add.image(400, 200, "background");
+    background.setScale(1.75);
+
+    //Player creation
+    player = this.physics.add.sprite(200, 450, 'idle');
+    player.setScale(2);
+    player.setBounce(0);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300)
+
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('idle', { start: 1, end: 9 }),
+        frameRate: 3,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('run', { start: 0, end: 10 }),
+        frameRate: 6
+    });
     
-        // add it to the stage and render!
-        app.stage.addChild(character);
-        character.setTransform(200,300,2,2);
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 8 }),
+        frameRate: 2,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'land',
+        frames: this.anims.generateFrameNumbers('land', { start: 0, end: 8 }),
+        frameRate: 2,
+        repeat: -1
+    });
+
+    //Bounds creation
+
+}
+
+function update ()
+{
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.up.isDown && player.body.checkWorldBounds()) {
+        player.setVelocityY(-330);
     }
-    function clearAnimations(){
-        const character = app.stage.getChildAt(0);
-        character.stop();
-        character.destroy();
-        app.stage.removeChild(character);
+    if(!player.body.checkWorldBounds()){
+        player.anims.play('jump', true);
     }
-    function jump(){
-                // get the sheet json data, required for resolving animations
-                const animations = PIXI.Assets.cache.get("assets/spritesheet.json").data.animations;
-                // create an animated sprite
-                const character = PIXI.AnimatedSprite.fromFrames(animations["jump"]);
-                // configure + start animation:
-                character.animationSpeed = 1 / 6;                     // 6 fps
-                character.play();
-            
-                // add it to the stage and render!
-                app.stage.addChild(character);
-
-                app.ticker.add(function(delta){
-                    var i = 0;
-                    character.setTransform(i ,300,2,2);
-                    i = i+1000;
-                });
-
-                
+    else{
+        player.anims.play("run", true);
     }
-    drawAnimation("idle");
-    jump();
-
-});
-
-
+}
