e312e147a5f0b3ee0668eead30cd47e4e71bb34f
pedrogg0 <pedrojosegarcia00@gmail.com>
2023-09-24 21:00:27 +0200

    spacebar imput implementation

--- js/startScene.js
+++ js/startScene.js
@@ -7,11 +7,35 @@ class StartScene extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/foreground.png');
+        this.load.spritesheet("idle", "assets/idle.png", { frameWidth: 48, frameHeight: 48 });
     }
 
     create() {
-        this.image = this.add.image(400, 300, 'background');
+        this.image = this.add.image(0, 0, 'background');
+        this.image.setOrigin(0,0);
+
+        //Animation for the character
+        this.anims.create({
+            key: "idle",
+            frameRate: 7,
+            frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 10}),
+        });
+
+        this.character = this.physics.add.sprite(250, 220, "idle");
+        this.character.setScale(1.5);
+        this.add.text(125, 125, 'Press space to start', { font: '40px Arial' });
+        this.input.keyboard.on('keydown-ENTER', function (event) {
+            console.log("hola");
+        });
+
     }
+
+    update(){
+        this.character.play("idle", true);
+
+    }
+
 }
+
 
 export default StartScene;
